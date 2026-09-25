09d4656436a93fd924cec788a8421405da8c36d6
andrewajmarks-prog <andrew.aj.marks@gmail.com>
2026-09-25 14:24:58 -0400

    Change Supabase URL and key
    Updated Supabase URL and key for the project.

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://YOURPROJECT.supabase.co";
-const SUPABASE_KEY = "sb_publishable_YOURKEY";
+const SUPABASE_URL = "https://gqtuupyqhgqbismnlssl.supabase.co";
+const SUPABASE_KEY = "sb_publishable_4tCAGKB8-k5rvfsFDeAlLA_iPWWbByI";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
