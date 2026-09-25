a66973f81fa02641c08b4e27bcbec1948461e238
andrewajmarks-prog <andrew.aj.marks@gmail.com>
2026-09-24 21:48:36 -0400

    Update app.js
    Connected to supabase

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const SUPABASE_URL = "https://gqtuupyqhgqbismnlssl.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_4tCAGKB8-k5rvfsFDeAlLA_iPWWbByI";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
 const playerInput = document.getElementById("playerInput");
 const addButton = document.getElementById("addButton");
 const playerList = document.getElementById("playerList");
